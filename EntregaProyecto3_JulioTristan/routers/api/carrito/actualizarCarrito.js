68cccd81ab9b668818ce8979883412cd933cee73
JulioT <julio.triztan01@gmail.com>
2023-01-11 22:11:42 -0600

    entregable 15

--- EntregaProyecto3_JulioTristan/routers/api/carrito/actualizarCarrito.js
+++ EntregaProyecto3_JulioTristan/routers/api/carrito/actualizarCarrito.js
@@ -3,6 +3,7 @@ import { carritosDao } from "../../../daos/index.js";
 import { productosDao,usuariosDao } from "../../../daos/index.js";
 import logger from "../../../logs/logger.js";
 import { base_host } from "../../../bin/www.js";
+import CarritosDaoArchivo from "../../../daos/carritos/CarritosDaoArchivo.js";
 
 
 const routeractualizarcarrito = Router();
@@ -17,23 +18,19 @@ routeractualizarcarrito.post("/:id/update", (req, res, next) => {
       return producto[0];
     });
     Promise.all(productos).then((listaProductos) => {
-      let data = {productos: listaProductos}
-      carritosDao
-        .actualizar(req.params.id, data)
-        .then((actualizar) => {
-          usuariosDao.buscar(req.session.username).then((value) => {
-            let avatar = value[0].avatar;
-            const username = {
-              username: {
-                username: req.session.username,
-                base_url: base_host,
-                avatar: avatar,
-              },
-            };
-            logger.info(actualizar);
-            res.render("menu", username);
-          });
-        });
+      console.log('esta es la data', listaProductos);
+      carritosDao.listar(req.params.id).then((carrito) => {
+        let listaCarrito = carrito[0].productos
+        console.log(listaCarrito)
+        let listaNueva = [];
+        for (let producto of listaProductos) {
+          for (let productoNuevo of listaCarrito){
+            if (producto._id.toString() === productoNuevo._id.toString()){
+              productoNuevo.cantidad = productoNuevo.cantidad+producto.cantidad
+            }
+          }
+        }
+      })
     });
   } catch (error) {
     logger.error(error);
